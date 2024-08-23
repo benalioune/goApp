5f5d1248834de6e6b87d5962a60d0fa39f4273c4
benalioune <benalioune6@gmail.com>
2024-08-23 20:27:40 +0200

    add database config

--- models/book.go
+++ models/book.go
@@ -1,8 +1,35 @@
 package models
 
+import (
+	"example/web-service-gin/database"
+)
+
 type Book struct {
 	ID     uint    `json:"id" gorm:"primaryKey"`
 	Title  string  `json:"title"`
 	Author string  `json:"author"`
 	Price  float64 `json:"price"`
+}
+
+func GetAllBooks() ([]Book, error) {
+	var books []Book
+	if err := database.DB.Find(&books).Error; err != nil {
+		return nil, err
+	}
+	return books, nil
+}
+
+func AddNewBook(book *Book) error {
+	if err := database.DB.Create(&book).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetBookByID(id uint) (*Book, error) {
+	var book Book
+	if err := database.DB.First(&book, id).Error; err != nil {
+		return nil, err
+	}
+	return &book, nil
 }
